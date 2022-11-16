d168fcd791aa489b75b4fba5256586951935b99b
Jack Rydzik <p23jrydzik@fairfieldprep.org>
2022-11-16 09:15:00 -0500

    finished lab 7-1

---
+++ lab_7-1.py
@@ -0,0 +1,13 @@
+# author: JHR 11/16/22
+
+# function for greeting, returning instead of printing
+def greeting():
+    '''prints: Hello World!'''
+    hi = "Hello World!"
+    return (hi)
+
+# prints the docstring and some other stuff (commented so it doesn't break the rest of the function)
+# help (greeting)
+
+# printing the result of the function
+print (greeting())
